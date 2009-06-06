868b212cfe37c45efac20a34f0c7e7167fbe4acd
Trey Piepmeier <trey@treypiepmeier.com>
2009-06-05 20:33:38 -0500

    First.

---
+++ readernaut.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WP-Readernaut
+Plugin URI: http://github.com/trey/wp-readernaut/
+Description: Show your Readernaut books on your WordPress site.
+Author: Trey Piepmeier
+Version: 1.0
+Author URI: http://treypiepmeier.com/
+*/
+
+?>
