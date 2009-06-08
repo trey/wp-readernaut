736d2f5f8280686aeede621eecb9a94db62ab862
Trey Piepmeier <trey@treypiepmeier.com>
2009-06-08 07:26:14 -0500

    2.7 style thanks to settings_fields.
    Read further down the page: http://codex.wordpress.org/Creating_Options_Pages
    
    This didn't make much sense to me: http://codex.wordpress.org/Settings_API

--- readernaut.php
+++ readernaut.php
@@ -8,7 +8,14 @@ Version: 1.0
 Author URI: http://treypiepmeier.com/
 */
 
-add_action('admin_menu', 'readernaut_menu');
+if (is_admin()) {
+	add_action('admin_menu', 'readernaut_menu');
+	add_action('admin_init', 'register_readernaut');
+}
+
+function register_readernaut() {
+	register_setting('readernaut_group', 'readernaut_username');
+}
 
 function readernaut_menu() {
 	add_options_page('Readernaut Options', 'Readernaut', 8, __FILE__, 'readernaut_options');
@@ -19,14 +26,12 @@ function readernaut_options() {
 	<div class="wrap">
 		<h2>Readernaut</h2>
 		<form method="post" action="options.php">
-			<?php wp_nonce_field('update-options'); ?>
+			<?php settings_fields('readernaut_group'); ?>
 			<table class="form-table">
 				<tr valign="top">
 					<th scope="row"><?php _e('Readernaut Username'); ?></th>
 					<td>
 						<input type="text" name="readernaut_username" value="<?php echo get_option('readernaut_username'); ?>" />
-						<input type="hidden" name="action" value="update" />
-						<input type="hidden" name="page_options" value="readernaut_username" />
 					</td>
 				</tr>
 			</table>
